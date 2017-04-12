dd204693deba3a348860c76bdc2d465a1087fb7e
Holvixd <vilhosten@gmail.com>
2017-04-12 13:47:07 +0300

    Add frontpage
    Add frontpage

--- web/js/addScreen.js
+++ web/js/addScreen.js
@@ -71,7 +71,7 @@ console.log(JSON.stringify(data).length);
           <input type="number" name="weight" defaultValue="15.65"/>
           <br/>
           Picture:<br/>
-          <input type="text" name="picture" defaultValue="http://pic-url-here.jpg"/>
+          <input type="text" name="picture" defaultValue="https://images.jimms.fi/product/4/1/145548-p441729.jpg"/>
           <br/>
         <input type="submit" value="Submit" />
       </form>
