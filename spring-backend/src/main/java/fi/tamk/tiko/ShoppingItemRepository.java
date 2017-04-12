4df15d4643bfa56b59ea31d89d9050c0a374f05a
Eerik <eerik.timonen94@gmail.com>
2017-04-12 16:55:11 +0300

    -Add update
    -Add update for item

--- spring-backend/src/main/java/fi/tamk/tiko/ShoppingItemRepository.java
+++ spring-backend/src/main/java/fi/tamk/tiko/ShoppingItemRepository.java
@@ -1,8 +1,17 @@
 
 package fi.tamk.tiko;
 
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ShoppingItemRepository extends CrudRepository<ShoppingItem, Long> {
-
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+    
+    @Modifying
+    @Query("Update ShoppingItem t SET t.price=:price WHERE t.id=:id")
+    @Transactional
+    public void updateTitle(@Param("id") Long id, @Param("price") Double price);
 }
