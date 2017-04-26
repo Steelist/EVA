b34ce4abadd5e557c934169f0f4caf2e2b51f814
Aleksi <alekhell@hotmail.fi>
2017-04-26 20:56:04 +0300

    Improves readability and integrity
    Improves readability by arranging the code properly. Missing files were
    added.

--- spring-backend/src/main/resources/static/js/addCp.js
+++ spring-backend/src/main/resources/static/js/addCp.js
@@ -1,79 +1,81 @@
 class ComputerForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+    handleSubmit(event) {
+        alert('A computer was submitted: ');
+        event.preventDefault();
+        var frm = $(document.myform);
+        var data = getFormData(frm);
+        console.log(data);
 
-  handleSubmit(event) {
-    alert('A computer was submitted: ');
-    event.preventDefault();
-    var frm = $(document.myform);
-    var data = getFormData(frm);
-    console.log(data);
-    fetch('http://localhost:8080/computer', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(data),
-        mode: "cors"
-})
+        fetch('http://localhost:8080/computer', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            mode: "cors"
+        })
 
-console.log(JSON.stringify(data).length);
+        console.log(JSON.stringify(data).length);
+    }
 
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} name="myform">
-          Name:<br/>
-          <input type="text" name="name" defaultValue="CasualGamer v0.5"/>
-          <br/>
-          Price tag:<br/>
-          <input type="number" name="price" defaultValue="1999.99"/>
-          <br/>
-          CPU:<br/>
-          <input type="text" name="cpu" defaultValue="Intel i7-7700K"/>
-          <br/>
-          GPU:<br/>
-          <input type="text" name="gpu" defaultValue="Asus Strix 1080ti OC"/>
-          <br/>
-          PSU:<br/>
-          <input type="text" name="psu" defaultValue="Crucial 750W modular"/>
-          <br/>
-          RAM:<br/>
-          <input type="text" name="ram" defaultValue="Ballistix 2x8gb 2666mhz"/>
-          <br/>
-          HDD:<br/>
-          <input type="text" name="hdd" defaultValue="Western Digital 1tb 7600rpm"/>
-          <br/>
-          Motherboard:<br/>
-          <input type="text" name="motherBoard" defaultValue=" ASROG Maximus Hero VIII"/>
-          <br/>
-          Weight:<br/>
-          <input type="number" name="weight" defaultValue="20.55"/>
-          <br/>
-          Operating System:<br/>
-          <input type="text" name="os" defaultValue="Windows 10 pro"/>
-          <br/>
-          Picture:<br/>
-          <input type="text" name="picture" defaultValue="https://images.jimms.fi/product/6/4/153965-b339765_300x300.jpg"/>
-          <br/>
-          Quantity:<br/>
-          <input type="number" name="quantity" defaultValue="10"/>
-          <br/>
-        <input type="submit" value="Submit" />
-      </form>
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} name="myform">
+            Name:<br/>
+        <input type="text" name="name" defaultValue="CasualGamer v0.5"/>
+            <br/>
+            Price tag:<br/>
+        <input type="number" name="price" defaultValue="1999.99"/>
+            <br/>
+            CPU:<br/>
+        <input type="text" name="cpu" defaultValue="Intel i7-7700K"/>
+            <br/>
+            GPU:<br/>
+        <input type="text" name="gpu" defaultValue="Asus Strix 1080ti OC"/>
+            <br/>
+            PSU:<br/>
+        <input type="text" name="psu" defaultValue="Crucial 750W modular"/>
+            <br/>
+            RAM:<br/>
+        <input type="text" name="ram" defaultValue="Ballistix 2x8gb 2666mhz"/>
+            <br/>
+            HDD:<br/>
+        <input type="text" name="hdd" defaultValue="Western Digital 1tb 7600rpm"/>
+            <br/>
+            Motherboard:<br/>
+        <input type="text" name="motherBoard" defaultValue=" ASROG Maximus Hero VIII"/>
+            <br/>
+            Weight:<br/>
+        <input type="number" name="weight" defaultValue="20.55"/>
+            <br/>
+            Operating System:<br/>
+        <input type="text" name="os" defaultValue="Windows 10 pro"/>
+            <br/>
+            Picture:<br/>
+        <input type="text" name="picture" defaultValue="https://images.jimms.fi/product/6/4/153965-b339765_300x300.jpg"/>
+            <br/>
+            Quantity:<br/>
+        <input type="number" name="quantity" defaultValue="10"/>
+            <br/>
+            Tags:<br/>
+        <input type="text" name="tags" defaultValue="CasualGamer v0.5 tehomylly Intel i7-7700K 1080ti OC Windows 10 16gb RAM 750W psu"/>
+            <br/>
+            <input type="submit" value="Submit" />
+            </form>
     );
-  }
+    }
 }
 
 function getFormData($form){
@@ -88,6 +90,6 @@ function getFormData($form){
 }
 
 ReactDOM.render(
-            <ComputerForm/>,
-            document.getElementById('root')
-        );
+<ComputerForm/>,
+    document.getElementById('root')
+);
---
+++ spring-backend/target/classes/static/js/smallSearch.js
@@ -0,0 +1,122 @@
+class StampForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.state = {data: ["asd"]}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+    showResult(response) {
+
+            this.setState({
+                data: response
+            });
+    }
+
+
+  getDataFromServer(URL){
+  var frm = $(document.myform);
+    var data = getFormData(frm);
+        $.ajax({
+            type:"POST",
+            dataType:"json",
+            headers: {
+            'content-type': 'application/json',
+        },
+            url: URL,
+            data: JSON.stringify(data),
+            success: function(response) {
+                this.showResult(response);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.getDataFromServer('http://localhost:8080/itemsSearch');
+    var frm = $(document.myform);
+    var data = getFormData(frm);
+    }
+
+
+
+
+
+  render() {
+    return (
+    <div>
+      <form onSubmit={this.handleSubmit} name="myform">
+          <br/>
+          <input type="text" name="tags" placeholder="Press enter to search..."></input>
+
+      </form>
+
+      <Result result={this.state.data}/>
+
+      </div>
+    );
+  }
+}
+
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
+
+
+var Result = React.createClass({
+    render:function(){
+        var result = this.props.result.map(function(result,index){
+            return <ResultItem key={index} user={ result } />
+            });
+        return(
+            <div className="container">
+                <div className="row">
+                    {result}
+                </div>
+            </div>
+        );
+    }
+});
+
+var ResultItem = React.createClass({
+    render:function(){
+        var camper = this.props.user;
+        if (camper.name){
+            return(
+                <div className="col-xs-6 col-sm-4 col-md-3">
+                <div className="item">
+                <div className="col-xs-12"><h3>{camper.name}</h3></div>
+                    <div className="col-xs-12"><img src={camper.picture} /></div>
+                    <div className="col-xs-12"><p>Hinta:&nbsp;{camper.price}</p></div>
+                    <div className="col-xs-12"><p>Paino:&nbsp;{camper.weight}</p></div>
+                </div>
+                </div>
+            );
+        }else{
+            return <div></div>
+        }
+    }
+});
+
+
+ReactDOM.render(
+            <StampForm/>,
+            document.getElementById('app2')
+        );
